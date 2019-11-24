8a3bf5372b1b55f591095a0441b6d914a75b676b
SlackEric <ericye1001@gmail.com>
2019-11-24 11:42:42 +1000

    replace username to email in auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,25 +2,23 @@ const User = require('../models/user');
 const { generateToken } = require('../utils/jwt');
 
 async function loginUser (req, res) {
-    const { username, password } = req.body;
+    const { email, password } = req.body;
 
-    const existingUser = await User.findOne({ username }).exec();
+    const existingUser = await User.findOne({ email }).exec();
+
     if (!existingUser) {
-        return res.status(401).json('Invalid username or password');
+        return res.status(401).json('Invalid email or password');
     }
 
     const validPassword = await existingUser.validatePassword(password);
 
     if (!validPassword) {
-        return res.status(401).json('Invalid username or password');
+        return res.status(401).json('Invalid email or password');
     }
-
+    console.log(existingUser);
     const token = generateToken(existingUser._id);
-    return res.json({ username, token });
+    return res.json({ email, token });
 }
-
-
-
 
 module.exports = {
     loginUser,
