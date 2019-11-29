7a3144f7170252f67b32ff29b79e7ffb84518d4a
Yuhua PU <yuhua.pu@uqconnect.uq.edu.au>
2019-11-29 23:44:15 +1000

    Update amd delete uses(students,tutors) working fine locally

--- src/models/student.js
+++ src/models/student.js
@@ -15,6 +15,7 @@ const schema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique: true,
         validate: {
             validator: email => !Joi.string().email().validate(email).error,
             msg: 'Invalid email format'
